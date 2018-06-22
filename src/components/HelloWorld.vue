44e3a3785467da151768d7856ea669c85745b7bd
Dan Sudenfield <dan@alipes.com>
2018-06-22 14:12:02 -0400

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <plaid 
+    env="sandbox"
+    :publicKey="publicKey"
+    clientName="Test App"
+    product="transactions"
+    v-bind="{ onSuccess }">
+    Open Plaid Link
+  </plaid>
+</template>
+
+<script>
+  import Plaid from './Plaid.vue';
+  export default {
+    name: 'PlaidLink',
+    data() {
+      return {
+        // publicKey: process.env.VUE_APP_PLAID_PUBLIC_KEY,
+        publicKey: '3fd48ca30df117a9ed536aa76bfb50'
+      }
+    },
+    components: {
+      Plaid
+    },
+    created() {
+      console.log(this.publicKey)
+    },
+    methods: {
+        onSuccess (token) {
+            console.log(token)
+        }
+    }
+  }
+</script>
